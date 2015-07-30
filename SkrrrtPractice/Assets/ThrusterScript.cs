1b14b26863a04dbaee0b1a6456da781dae90f93f
Max <mfp10c@my.fsu.edu>
2015-07-29 23:11:21 -0400

    HoverSkrrt
    This is the first installment weak bitch!!!!

---
+++ SkrrrtPractice/Assets/ThrusterScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrusterScript : MonoBehaviour {
+
+
+	public float thrusterStrength;
+	public float thrusterDistance;
+	public Transform[] thrusters;
+
+
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		RaycastHit hit;
+		foreach (Transform thruster in thrusters) 
+		{
+			Vector3 downwardForce;
+			float distancePercentage;
+		
+			if(Physics.Raycast (thruster.position, thruster.up * -1, out hit, thrusterDistance))
+			{
+				// The thruster is within thrusterDistance to the ground. How far away?
+				distancePercentage = 1 - (hit.distance/thrusterDistance);
+			
+				downwardForce = transform.up * thrusterStrength * distancePercentage;
+
+				// Calculate how much force to push
+				downwardForce = downwardForce * Time.deltaTime * GetComponent<Rigidbody>().mass;
+			
+				// Apply the force where the thruster is
+				GetComponent<Rigidbody>().AddForceAtPosition(downwardForce, thruster.position);
+			}
+		}
+	}
+}
